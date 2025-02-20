11e7f2f6cbc596612bc38474b8746aff9a1470f5
PrafulPradhan11 <Prafulpradhan1113@gmail.com>
2025-02-20 15:07:56 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,36 @@
+cup="coffee";
+
+// let 
+// var 
+// const
+
+// if(true){
+//     const name = "John Doe"
+//     console.log(name, "This is first one")
+// }
+// console.log(name, "This is second one")
+
+// const age = 21
+// const isNepali = true
+
+// let name = "Praful"
+// name = 123123
+// console.log(name + 10);
+
+// let friend1 = "Pranjal"
+// let friend2  = "Bob"
+// let friend3 = "Anup"
+
+
+let friends = ['Pranjal','Bob','Anup']
+// console.log(friends[1]);
+
+
+
+// let khoyo = ['DigitalPathasala',999,]
+let khoyo = {
+    instituteName: "DigitalPathshala",
+    reactCoursePrice: 999
+}
+console.log(khoyo.reactCoursePrice);
+//key:value
